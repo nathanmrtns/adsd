96a2632b8277d9efff4a137f32eb05adfae04934
Nathan <nathanmrtns@gmail.com>
2017-01-28 19:43:19 -0300

    VERSION 1 of app + automatizador

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import request
 from flask.ext.sqlalchemy import SQLAlchemy
 import psutil
+import time
+import csv
 
 app = Flask(__name__)
 #mysql url - mysql://[user]:[user password]@[localhost]/[database name]
@@ -16,31 +18,40 @@ class Registro(db.Model):
 @app.route('/write_and_read', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        import time
-
+        server_resp_timei = time.time()
         registro = Registro(texto='textinho')
-        print registro
-        ti = time.time()
+        bd_timei = time.time()
         db.session.add(registro)
         db.session.commit()
-        tf = time.time() - ti
+        bd_timef = time.time() - bd_timei
         registro = Registro.query.filter_by(texto='textinho').first()
         cpu_usage = psutil.cpu_percent(interval=0.1)
         if registro:
-            return str(tf) + " " + str(cpu_usage)
+            server_resp_timef = time.time() - server_resp_timei
+            self.write_data("write", server_resp_timef,bd_timef,cpu_usage)
+            return str(server_resp_timef) + " " + str(bd_timef) + " " + str(cpu_usage)
 
 @app.route('/read', methods=['GET', 'POST'])
 def read():
     if request.method == 'GET':
-        import time
-        ti = time.time()
+        server_resp_timei = time.time()
+        bd_timei = time.time()
         registro = Registro.query.filter_by(texto='textinho').first()
-        tf = time.time() - ti
+        bd_timef = time.time() - bd_timei
         cpu_usage = psutil.cpu_percent(interval=0.1)
         #print cpu_usage
         if registro:
-            return str(tf) + " " + str(cpu_usage)
+            server_resp_timef = time.time() - server_resp_timei
+            write_data("write", server_resp_timef,bd_timef,cpu_usage)
+            return str(server_resp_timef) + " " + str(bd_timef) + " " + str(cpu_usage)
         return "EMPTY"
+
+# salva os dados no arquivo definido
+def write_data(command, server_response, bd_response, cpu_usage):
+    fd = open("output", 'a')
+    output = csv.writer(fd, delimiter=' ')
+    output.writerow([command, server_response, bd_response, cpu_usage])
+    fd.close()
 
 if __name__ == '__main__':
     db.create_all()
--- automatizador.py
+++ automatizador.py
@@ -29,18 +29,22 @@ class TesterADSD(object):
         if not self.url:
             raise Exception("Target not set.")
         command = self.build_command()
-        for iteration in range(self.repetitions):
-            response = self.shoot(command)
-            time_elapsed = response[0]
-            bd_time = response[1].split(" ")[0]
-            cpu_usage = response[1].split(" ")[1]
-            if(command.split(' ')[6] == "--uri=/write_and_read"):
-                self.write_data(self.rate, time_elapsed, iteration, 'write', bd_time, cpu_usage)
-            else: self.write_data(self.rate, time_elapsed, iteration, 'read', bd_time, cpu_usage)
+        self.shoot(command)
+        # for iteration in range(self.repetitions):
+        #     response = self.shoot(command)
+        #     print response
+        #     time_elapsed = response#[0]
+        #     bd_time = " " #response[1].split(" ")[0]
+        #     cpu_usage = " " # response[1].split(" ")[1]
+        #     if(command.split(' ')[6] == "--uri=/write_and_read"):
+        #         self.write_data(self.rate, time_elapsed, iteration, 'write', bd_time, cpu_usage)
+        #     else: self.write_data(self.rate, time_elapsed, iteration, 'read', bd_time, cpu_usage)
+        print "done"
 
     def shoot(self, command):
         start_time = time.time()
         p = check_output(command.split(' '))
+        print p
         time_elapsed = time.time() - start_time
         bd_time = p.split("\n")[1].split(":")[1]
         return [time_elapsed, bd_time]
@@ -54,7 +58,7 @@ class TesterADSD(object):
         final_command = command.format(
             server=self.url,
             port=5000,
-            requests=self.rate,
+            requests=self.rate*60,
             rate=self.rate,
             uri=uri
         )
@@ -69,7 +73,7 @@ class TesterADSD(object):
         fd.close()
 
 if __name__ == '__main__':
-    tester = TesterADSD(1, 10, output='output.csv')
+    tester = TesterADSD(10, 1, output='output.csv')
     tester.set_target('127.0.0.1')
-    tester.set_mode(1) # 0 = leitura | 1 = escrita
+    tester.set_mode(0) # 0 = leitura | 1 = escrita
     tester.start()
