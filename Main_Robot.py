eb6d4a116b4367cd99641c2a6493060cd42cce30
Nathan Barreto <nathan.barreto@embedded.ufcg.edu.br>
2017-02-01 10:33:31 -0300

    small fix

--- Main_Robot.py
+++ Main_Robot.py
@@ -9,9 +9,9 @@ LEITURA = 0
 ESCRITA_E_LEITURA = 1
 
 class TesterADSD(object):
-    def __init__(self, rate, repetitions, output=None, mode=LEITURA):
+    def __init__(self, rate, req_per_min, output=None, mode=LEITURA):
         self.rate = rate
-        self.repetitions = repetitions
+        self.requests = req_per_min
         self.mode = mode
         self.output = output
         self.url = None
@@ -25,39 +25,40 @@ class TesterADSD(object):
     def start(self):
         if not self.url:
             raise Exception("Target not set.")
-        #command = self.build_command()
-        if(self.repetitions == 10):
-            self.paced_shooter(self.url, self.repetitions, self.mode, 6) #10 requests per minute
+        if(self.requests == 10):
+            self.paced_shooter(self.url, 1, self.requests, self.mode, 6) #10 requests per minute
         else:
-            self.paced_shooter(self.url, self.repetitions, self.mode, 0.24) #250 requests per minute
+            self.paced_shooter(self.url, 1, self.requests, self.mode, 0.24) #250 requests per minute
         print "done"
 
-    def paced_shooter(self, url, repetitions, command, wait_time):
-        counter = 0
-        while(counter <= repetitions):
-            conn = httplib.HTTPConnection(url)
+    def paced_shooter(self, url, observation_time, requests, command, wait_time):
+        #observation_time is in minutes
+        for i in range(observation_time):
+            counter = 0
+            while(counter < requests):
+                conn = httplib.HTTPConnection(url)
 
-            if(command == ESCRITA_E_LEITURA):
-                print "Write"
-                conn.request("GET", "/write_and_read")
-            else:
-                print "Read"
-                conn.request("GET", "/read")
+                if(command == ESCRITA_E_LEITURA):
+                    print "Write"
+                    conn.request("GET", "/write_and_read")
+                else:
+                    print "Read"
+                    conn.request("GET", "/read")
 
-            total_timei = time.time()
-            r1 = conn.getresponse()
-            total_timef = time.time() - total_timei
-            response = r1.read().split(" ")
-            time_elapsed = response[0]
-            bd_time = response[1]
-            cpu_usage = response[2]
+                total_timei = time.time()
+                r1 = conn.getresponse()
+                total_timef = time.time() - total_timei
+                response = r1.read().split(" ")
+                time_elapsed = response[0]
+                bd_time = response[1]
+                cpu_usage = response[2]
 
-            if(command == ESCRITA_E_LEITURA):
-                self.write_data('write', total_timef, time_elapsed, bd_time, cpu_usage)
-            else:
-                self.write_data('read',  total_timef, time_elapsed, bd_time, cpu_usage)
-            counter += 1
-            time.sleep(wait_time)
+                if(command == ESCRITA_E_LEITURA):
+                    self.write_data('write', total_timef, time_elapsed, bd_time, cpu_usage)
+                else:
+                    self.write_data('read',  total_timef, time_elapsed, bd_time, cpu_usage)
+                counter += 1
+                time.sleep(wait_time)
 
     # saves the data
     def write_data(self, command, total_time, server_response, bd_response, cpu_usage):
@@ -67,7 +68,7 @@ class TesterADSD(object):
         fd.close()
 
 if __name__ == '__main__':
-    tester = TesterADSD(1, 5, output='output.csv')
+    tester = TesterADSD(1, 10, output='output.csv')
     tester.set_target('127.0.0.1:5000')
     tester.set_mode(1) # 0 = leitura | 1 = escrita
     tester.start()
--- README.md
+++ README.md
@@ -9,14 +9,7 @@ This simple app returns the following stats about the server:
  - DB response time
  - CPU usage %
 
-There are two versions of the server and two versions of the robot.
-Use server 1 with robot 1 and server 2 with robot 2.
-
-Server and Robot 1 Behavior:
- - Server writes the stats on file.
- - Robot used to set the rate | number of requests sent to server
-
-Server and Robot 2 Behavior:
+Server and Robot Behavior:
  - Server just receives and respond the requests
  - This robot's behavior depends of the number of iterations, there are 2 options:
      - Requests are sent at 10 requests/minute
